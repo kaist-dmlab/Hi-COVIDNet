244ae8ab8ea632b5175660fc243b623ca7a65a51
MinSeokKim <minseokkim@kaist.ac.kr>
2020-08-24 17:25:17 +0900

    Update covid_aux.py

--- covid_aux.py
+++ covid_aux.py
@@ -310,7 +310,6 @@ class TransformerModel(nn.Module):
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers,).to(torch.float32)
         self.ninp = ninp
 
-#         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
 #         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
 #         return mask
 
